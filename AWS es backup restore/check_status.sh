fb3c96bb63f43b87946db1ba789450ed209fb99e
kumargan <kumar.ganesh242@gmail.com>
2018-11-23 15:59:17 +0530

    just like that

--- AWS es backup restore/check_status.sh
+++ AWS es backup restore/check_status.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-  status=`curl -XGET 'https://search--tvzve6vt3m4u3e7flmid5id7ui.us-east-1.es.amazonaws.com/_snapshot/es-index-backups/_all?pretty' | jq '.snapshots[0].state'`
+  status=`curl -XGET 'https://id5id7ui.us-east-1.es.amazonaws.com/_snapshot/es-index-backups/_all?pretty' | jq '.snapshots[0].state'`
   echo "status is $status"
   if [ "$status" == "\"IN_PROGRESS\"" ]; then
   	echo "not completed "
